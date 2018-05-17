f4ab2d9936df04b9637b3f59ad10bfe294ed0595
hatanoyuki <yunki.ok@icloud.com>
2018-05-17 22:33:17 +0900

    second commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>test1</title>
+    </head>
+    <body>
+        <?php
+            $a = $b = $c = $d = 2;
+            print $a * $b * $c * $d;
+        ?>
+    </body>
+</html>
